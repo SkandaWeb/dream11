d06d79132e4df819c03fdbc5248c137d0c0f76ed
QpayIndia <mani@qpayindia.com>
2025-01-17 08:55:41 +0530

    Commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2428674166"
+    serviceWorkerVersion: "1200766226"
   }
 });
